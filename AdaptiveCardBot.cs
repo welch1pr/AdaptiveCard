f30dad8e0e81ffb68cfdc6c82e67179e9d25958a
Welch <46856150+welch1pr@users.noreply.github.com>
2019-01-21 22:41:58 -0500

    Add project files.

---
+++ AdaptiveCardBot.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace AdaptiveCard
+{
+    /// <summary>
+    /// Represents a bot that processes incoming activities.
+    /// For each user interaction, an instance of this class is created and the OnTurnAsync method is called.
+    /// This is a Transient lifetime service. Transient lifetime services are created
+    /// each time they're requested. Objects that are expensive to construct, or have a lifetime
+    /// beyond a single turn, should be carefully managed.
+    /// For example, the <see cref="MemoryStorage"/> object and associated
+    /// <see cref="IStatePropertyAccessor{T}"/> object are created with a singleton lifetime.
+    /// </summary>
+    /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-2.1"/>
+    public class AdaptiveCardBot : IBot
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>                        
+        public AdaptiveCardBot()
+        {
+        }
+
+        /// <summary>
+        /// Every conversation turn calls this method.
+        /// </summary>
+        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
+        /// for processing this conversation turn. </param>
+        /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
+        /// <seealso cref="BotStateSet"/>
+        /// <seealso cref="ConversationState"/>
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                Attachment attachment = null;
+                switch (turnContext.Activity.Text.ToUpper())
+                {
+                    case "FLIGHT":
+                        attachment = CreateAdaptiveCardAttachment(@".\Resources\flight.json");
+                        break;
+                    case "WEATHER":
+                        attachment = CreateAdaptiveCardAttachment(@".\Resources\weather.json");
+                        break;
+                    case "RESTAURANT":
+                        attachment = CreateAdaptiveCardAttachment(@".\Resources\restaurant.json");
+                        break;
+                }
+
+                if (attachment != null)
+                {
+                    var reply = turnContext.Activity.CreateReply();
+                    reply.Attachments = new List<Attachment>() { attachment };
+                    await turnContext.SendActivityAsync(reply, cancellationToken);
+                }
+                else
+                {
+                    await turnContext.SendActivityAsync("Please type one of the following to see an Adaptive Card: flight, weather, restaurant");
+                }
+            }
+        }
+
+        private static Attachment CreateAdaptiveCardAttachment(string filePath)
+        {
+            var adaptiveCardJson = File.ReadAllText(filePath);
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+    }
+}
